97c3837886c5f40eb2dada4bdc16069cbbddea58
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-29 14:05:56 +0100

    creato il file home.blade.php ed eliminato welcome.blade.php (di default)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage-LARAVEL</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
